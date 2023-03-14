7a869b4433f58592086f45646cfab11f1b28f7b5
Ângelo <angelo.pralves@gmail.com>
2023-03-14 14:09:06 -0300

    first routing and modules adaptations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
+import { appRoutes } from './app.routing';
+import { HomeComponent } from './home/home.component';
 
 import { MenuComponent } from './menu/menu.component';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { OpenAccessModule } from './open-access/open-access.module';
-import { ShopModule } from './shop/shop.module';
-import { EventsModule } from './events/events.module';
-import { ContactModule } from './contact/contact.module';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => HomeModule },
-  { path: 'about', loadChildren: () => AboutModule },
-  { path: 'open-access', loadChildren: () => OpenAccessModule },
-  { path: 'shop', loadChildren: () => ShopModule },
-  { path: 'events', loadChildren: () => EventsModule },
-  { path: 'contact', loadChildren: () => ContactModule }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
